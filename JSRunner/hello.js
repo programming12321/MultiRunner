f087d582a361bd99804dbdac0d107374fd44b324
programming12321 <joaquin.adones.v@colegiosantamariaeufrasia.cl>
2026-09-09 13:28:05 -0300

    Add hello.js with basic JavaScript examples

---
+++ JSRunner/hello.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+let x = 10;
+let y = 20;
+
+console.log(x + y);
+
+if (x < y) {
+    console.log("x is smaller");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+function square(x) {
+    return x * x;
+}
+
+console.log(square(5));
+
+console.log(Math.sqrt(144));
+console.log(Math.PI);
